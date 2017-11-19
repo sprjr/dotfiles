70b7dab4df64f3f17b8c3c1ba014f82b5538a68e
Stephen Rivas Jr <sprjr@me.com>
2017-11-19 11:01:51 -0800

    make dotfiles importable from other directories

--- .git.sh
+++ .git.sh
@@ -1,6 +1,6 @@
 # include git completion & prompt scripts
-source git/.gitcompletion.sh
-source git/.gitprompt.sh
+source $dotfiles/git/.gitcompletion.sh
+source $dotfiles/git/.gitprompt.sh
 
 # set terminal display
 export PS1="\h \[$(tput sgr0)\]\[\033[38;5;7m\][\[$(tput bold)\]\[$(tput sgr0)\]\[\033[38;5;39m\]\W\[\033[38;5;7m\]]\$(__git_ps1) \[$(tput sgr0)\]\[\033[38;5;160m\]>\[$(tput sgr0)\]\[\033[38;5;15m\] \[$(tput sgr0)\]"
