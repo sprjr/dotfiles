4302c4561c88b647c1562031c932ed56bbd104a9
Stephen Rivas Jr <sprjr@me.com>
2017-11-19 10:50:51 -0800

    reorganizing things makes them...better?

---
+++ .alias.sh
@@ -0,0 +1,7 @@
+# fave aliases
+alias ls='ls -al'
+
+# git aliases
+alias gcm='git checkout master'
+alias gcd='git checkout dev'
+alias gca='git commit --amend'
--- .git.sh
+++ .git.sh
@@ -1,12 +1,6 @@
-# fave aliases
-alias ls='ls -al'
-alias gcm='git checkout master'
-alias gcd='git checkout dev'
-alias gca='git commit --amend'
-
 # include git completion & prompt scripts
-source .gitcompletion.sh
-source .gitprompt.sh
+source git/.gitcompletion.sh
+source git/.gitprompt.sh
 
 # set terminal display
 export PS1="\h \[$(tput sgr0)\]\[\033[38;5;7m\][\[$(tput bold)\]\[$(tput sgr0)\]\[\033[38;5;39m\]\W\[\033[38;5;7m\]]\$(__git_ps1) \[$(tput sgr0)\]\[\033[38;5;160m\]>\[$(tput sgr0)\]\[\033[38;5;15m\] \[$(tput sgr0)\]"
