de630fcfda8d0cd8756cf2874ffdff0b0c2fac8c
Stephen Rivas Jr <sprjr@me.com>
2020-01-08 08:47:02 -0500

    latest updates to transfer to new workbook

--- .git.sh
+++ .git.sh
@@ -1,6 +1,6 @@
 # include git completion & prompt scripts
-source $dotfiles/git/.gitcompletion.sh
-source $dotfiles/git/.gitprompt.sh
+source $DOTFILES/git/.gitcompletion.sh
+source $DOTFILES/git/.gitprompt.sh
 
 # set terminal display
 export PS1="\h \[$(tput sgr0)\]\[\033[38;5;7m\][\[$(tput bold)\]\[$(tput sgr0)\]\[\033[38;5;39m\]\W\[\033[38;5;7m\]]\$(__git_ps1) \[$(tput sgr0)\]\[\033[38;5;160m\]>\[$(tput sgr0)\]\[\033[38;5;15m\] \[$(tput sgr0)\]"
--- zsh/paths.zsh
+++ zsh/paths.zsh
@@ -1,2 +1,5 @@
 # i did what yarn docs said to, no clue why
 export PATH="$(yarn global bin):$PATH"
+
+# Added by n-install (see http://git.io/n-install-repo).
+export N_PREFIX="$HOME/n"; [[ :$PATH: == *":$N_PREFIX/bin:"* ]] || PATH+=":$N_PREFIX/bin"
